0e42618dff1b77a2d51122d4338e4817862cb9b0
BobsunPower <bobsunpower@gmail.com>
2022-06-18 09:05:45 +0300

    Completed

---
+++ c_basics/FirstStepsInCoding/InchestoCentimeters/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InchestoCentimeters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(double.Parse(Console.ReadLine()) * 2.54);
+        }
+    }
+}
