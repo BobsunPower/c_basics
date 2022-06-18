ea85a527fc44d13b82512d14df2025905d4336ae
BobsunPower <bobsunpower@gmail.com>
2022-06-18 09:26:44 +0300

    Completed

---
+++ c_basics/FirstStepsInCoding/ConcatenateData/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConcatenateData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ c_basics/FirstStepsInCoding/VacationBooksList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VacationBooksList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
