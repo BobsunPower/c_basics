2000d759fcb0435d915502d896b2d7399954ae9c
BobsunPower <bobsunpower@gmail.com>
2022-06-19 13:34:39 +0300

    Completed

--- c_basics/FirstStepsInCoding/FoodDelivery/Program.cs
+++ c_basics/FirstStepsInCoding/FoodDelivery/Program.cs
@@ -6,7 +6,7 @@ namespace FoodDelivery
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine((double.Parse(Console.ReadLine()) * 10.35 + double.Parse(Console.ReadLine()) * 12.4 + double.Parse(Console.ReadLine()) * 8.15) * 1.2 + 2.5);
         }
     }
 }
