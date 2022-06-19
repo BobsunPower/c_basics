54a1e49ad9129833dc1e04960193114eccf8b83b
BobsunPower <bobsunpower@gmail.com>
2022-06-19 10:16:11 +0300

    Completed

--- c_basics/FirstStepsInCoding/SuppliesForSchool/Program.cs
+++ c_basics/FirstStepsInCoding/SuppliesForSchool/Program.cs
@@ -6,7 +6,7 @@ namespace SuppliesForSchool
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine((double.Parse(Console.ReadLine()) * 5.8 + double.Parse(Console.ReadLine()) * 7.2 + double.Parse(Console.ReadLine()) * 1.2) * ((100 - double.Parse(Console.ReadLine())) / 100));
         }
     }
 }
