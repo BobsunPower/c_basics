dc746416d1fc2a0d90c80d89e98ce1f3bd1a240d
BobsunPower <bobsunpower@gmail.com>
2022-06-19 13:25:16 +0300

    Completed

--- c_basics/FirstStepsInCoding/Repainting/Program.cs
+++ c_basics/FirstStepsInCoding/Repainting/Program.cs
@@ -6,7 +6,8 @@ namespace Repainting
     {
         static void Main(string[] args)
         {
-            
+            double expenses = (double.Parse(Console.ReadLine()) + 2) * 1.5 + double.Parse(Console.ReadLine()) * 1.1 * 14.5 + double.Parse(Console.ReadLine()) * 5 + 0.4;
+            Console.WriteLine(expenses + (expenses * 0.3 * double.Parse(Console.ReadLine())));
         }
     }
 }
