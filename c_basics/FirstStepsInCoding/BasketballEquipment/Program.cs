ef788cf147382a188c556f8da05cfc052296a790
BobsunPower <bobsunpower@gmail.com>
2022-06-19 16:43:34 +0300

    Completed

--- c_basics/FirstStepsInCoding/BasketballEquipment/Program.cs
+++ c_basics/FirstStepsInCoding/BasketballEquipment/Program.cs
@@ -6,7 +6,12 @@ namespace BasketballEquipment
     {
         static void Main(string[] args)
         {
-            
+            double sub = double.Parse(Console.ReadLine());
+            double shoes = sub * 0.6;
+            double kit = shoes * 0.8;
+            double ball = kit * 0.25;
+            double other = ball * 0.2;
+            Console.WriteLine(sub + shoes + kit + ball + other);
         }
     }
 }
--- c_basics/FirstStepsInCoding/FishTank/Program.cs
+++ c_basics/FirstStepsInCoding/FishTank/Program.cs
@@ -6,7 +6,7 @@ namespace FishTank
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine(((double.Parse(Console.ReadLine()) * double.Parse(Console.ReadLine()) * double.Parse(Console.ReadLine())) * 0.001) * (1 - double.Parse(Console.ReadLine()) / 100));
         }
     }
 }
