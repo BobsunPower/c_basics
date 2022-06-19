db1677a697a603427c8e5356ba042332aff83aea
BobsunPower <bobsunpower@gmail.com>
2022-06-19 10:02:28 +0300

    Completed

--- c_basics/FirstStepsInCoding/DepositCalculator/Program.cs
+++ c_basics/FirstStepsInCoding/DepositCalculator/Program.cs
@@ -6,7 +6,10 @@ namespace DepositCalculator
     {
         static void Main(string[] args)
         {
-            
+            double deposit = double.Parse(Console.ReadLine());
+            int months= int.Parse(Console.ReadLine());
+            double interest = double.Parse(Console.ReadLine()) / 100;
+            Console.WriteLine(deposit + deposit * interest / 12 * months);
         }
     }
 }
