105e9d2d7c4608c01cb0e5fee6e3a5898f0c8ec6
BobsunPower <bobsunpower@gmail.com>
2022-06-21 09:17:47 +0300

    Completed

--- c_basics/ConditionalStatements/ExcellentResult/Program.cs
+++ c_basics/ConditionalStatements/ExcellentResult/Program.cs
@@ -6,7 +6,8 @@ namespace ExcellentResult
     {
         static void Main(string[] args)
         {
-            
+            if (double.Parse(Console.ReadLine()) >= 5.50)
+            { Console.WriteLine("Excellent!"); }
         }
     }
 }
