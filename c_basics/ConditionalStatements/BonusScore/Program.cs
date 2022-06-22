293d4b3c0f3e4e57d48c130a6c701396c7b8f32b
BobsunPower <bobsunpower@gmail.com>
2022-06-22 11:52:04 +0300

    Completed

--- c_basics/ConditionalStatements/BonusScore/Program.cs
+++ c_basics/ConditionalStatements/BonusScore/Program.cs
@@ -6,7 +6,14 @@ namespace BonusScore
     {
         static void Main(string[] args)
         {
-            
+            int points = int.Parse(Console.ReadLine());
+            double bonusPoints = 0;
+            if (points <= 100) {bonusPoints += 5;}
+            else if (points < 1000) {bonusPoints = points * 0.20;}
+            else {bonusPoints = points * 0.10;}
+            if (points % 2 == 0) {bonusPoints++;}
+            else if (points % 10 == 5) {bonusPoints += 2;}
+            Console.WriteLine($"{bonusPoints}\n{bonusPoints + points}");
         }
     }
 }
