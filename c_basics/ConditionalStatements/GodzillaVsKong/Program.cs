3b24ab09b47c6de30276c0d44c2c96012ac3afed
BobsunPower <bobsunpower@gmail.com>
2022-06-23 14:13:58 +0300

    Completed

--- c_basics/ConditionalStatements/GodzillaVsKong/Program.cs
+++ c_basics/ConditionalStatements/GodzillaVsKong/Program.cs
@@ -6,7 +6,15 @@ namespace GodzillaVsKong
     {
         static void Main(string[] args)
         {
-            
+            double budget = double.Parse(Console.ReadLine());
+            int people = int.Parse(Console.ReadLine());
+            double clothes = people * double.Parse(Console.ReadLine());
+            double decor = budget * 0.1;
+            if (people >= 150) {double discount = clothes * 0.1; clothes -= discount;}
+            double expenses = decor + clothes;
+            if (expenses > budget) {Console.WriteLine($"Not enough money!\nWingard needs {expenses - budget:f2} leva more.");}
+            else {Console.WriteLine($"Action!\nWingard starts filming with {budget - expenses:f2} leva left.");
+            }
         }
     }
 }
