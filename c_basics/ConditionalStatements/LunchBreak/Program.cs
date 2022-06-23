2f67c44ab2a5d22c2d37de8e9640acae1a539c75
BobsunPower <bobsunpower@gmail.com>
2022-06-23 16:17:38 +0300

    Completed

--- c_basics/ConditionalStatements/LunchBreak/Program.cs
+++ c_basics/ConditionalStatements/LunchBreak/Program.cs
@@ -6,7 +6,12 @@ namespace LunchBreak
     {
         static void Main(string[] args)
         {
-            
+            string movie = Console.ReadLine();
+            int time = int.Parse(Console.ReadLine());
+            int rest = int.Parse(Console.ReadLine());
+            double free = rest - rest * 0.125 - rest * 0.25;
+            if (free >= time) {Console.WriteLine($"You have enough time to watch {movie} and left with {Math.Ceiling(free - time)} minutes free time.");}
+            else {Console.WriteLine($"You don't have enough time to watch {movie}, you need {Math.Ceiling(time - free)} more minutes.");}
         }
     }
 }
