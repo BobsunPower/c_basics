1e91ff0c87a85f28506a41a2b07d2d6b7ece3cc2
BobsunPower <bobsunpower@gmail.com>
2022-06-23 14:20:26 +0300

    Completed

--- c_basics/ConditionalStatements/WorldSwimmingRecord/Program.cs
+++ c_basics/ConditionalStatements/WorldSwimmingRecord/Program.cs
@@ -6,7 +6,11 @@ namespace WorldSwimmingRecord
     {
         static void Main(string[] args)
         {
-            
+            double record = double.Parse(Console.ReadLine());
+            double meters = double.Parse(Console.ReadLine());
+            double time = meters * double.Parse(Console.ReadLine()) + Math.Floor(meters / 15) * 12.5;
+            if (record > time) {Console.WriteLine($"Yes, he succeeded! The new world record is {time:f2} seconds.");}
+            else {Console.WriteLine($"No, he failed! He was {time - record:f2} seconds slower.");}
         }
     }
 }
