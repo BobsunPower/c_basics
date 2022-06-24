d8c8cf0b7dbfebbcda4c8e551835e3b0287c92de
BobsunPower <bobsunpower@gmail.com>
2022-06-24 09:30:24 +0300

    Completed

--- c_basics/ConditionalStatementsAdvanced/AnimalType/Program.cs
+++ c_basics/ConditionalStatementsAdvanced/AnimalType/Program.cs
@@ -6,7 +6,13 @@ namespace AnimalType
     {
         static void Main(string[] args)
         {
-            
+            string animal = Console.ReadLine();
+            switch (animal)
+            {
+                case "dog": Console.WriteLine("mammal"); break;
+                case "crocodile": case "tortoise": case "snake": Console.WriteLine("reptile"); break;
+                default: Console.WriteLine("unknown"); break;
+            }
         }
     }
 }
