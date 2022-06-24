52c80cc8261db2f1d31612ca4cd44396583967c8
BobsunPower <bobsunpower@gmail.com>
2022-06-24 13:20:50 +0300

    Completed

--- c_basics/ConditionalStatementsAdvanced/NumberInRange/Program.cs
+++ c_basics/ConditionalStatementsAdvanced/NumberInRange/Program.cs
@@ -6,7 +6,9 @@ namespace NumberInRange
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine());
+            if (n >= -100 && n <= 100 && n != 0) {Console.WriteLine("Yes");}
+            else {Console.WriteLine("No");}
         }
     }
 }
