1048fae9dbcf0c52bb72d3e99060a8211a619b46
BobsunPower <bobsunpower@gmail.com>
2022-06-24 09:30:49 +0300

    Completed

--- c_basics/ConditionalStatementsAdvanced/PersonalTitles/Program.cs
+++ c_basics/ConditionalStatementsAdvanced/PersonalTitles/Program.cs
@@ -6,7 +6,18 @@ namespace PersonalTitles
     {
         static void Main(string[] args)
         {
-            
+            double age = double.Parse(Console.ReadLine());
+            char sex = char.Parse(Console.ReadLine());
+            if (age >= 16)
+            {
+                if (sex == 'm') { Console.WriteLine("Mr."); }
+                else { Console.WriteLine("Ms."); }
+            }
+            else
+            {
+                if (sex == 'm') { Console.WriteLine("Master"); }
+                else { Console.WriteLine("Miss"); }
+            }
         }
     }
 }
