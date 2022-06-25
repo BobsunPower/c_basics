696b048da4ec60b4896dcb45f2a06d472591298c
BobsunPower <bobsunpower@gmail.com>
2022-06-25 21:41:01 +0300

    Completed

--- c_basics/ConditionalStatementsAdvanced/WorkingHours/Program.cs
+++ c_basics/ConditionalStatementsAdvanced/WorkingHours/Program.cs
@@ -6,7 +6,12 @@ namespace WorkingHours
     {
         static void Main(string[] args)
         {
-            
+            int hour = int.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+            if (hour >= 10 && hour <= 18)
+            {switch (day) { case "Monday": case "Tuesday": case "Wednesday": case "Thursday": case "Friday": case "Saturday":
+                        Console.WriteLine("open"); return;}}
+            Console.WriteLine("closed");
         }
     }
 }
