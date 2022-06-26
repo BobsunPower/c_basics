bf7b732f06458658a9cf3d54e7a9101974ea72e1
BobsunPower <bobsunpower@gmail.com>
2022-06-26 07:57:55 +0300

    Completed

--- c_basics/ConditionalStatementsAdvanced/TradeCommissions/Program.cs
+++ c_basics/ConditionalStatementsAdvanced/TradeCommissions/Program.cs
@@ -6,7 +6,32 @@ namespace TradeCommissions
     {
         static void Main(string[] args)
         {
-            
+            string city = Console.ReadLine();
+            double sells = double.Parse(Console.ReadLine());
+            double commision = 0;
+            if (sells <= 500) {
+                switch (city) {
+                    case "Sofia": commision = 0.05; break;
+                    case "Varna": commision = 0.045; break;
+                    case "Plovdiv": commision = 0.055; break;}}
+            else if (sells <= 1000) {
+                switch (city) {
+                    case "Sofia": commision = 0.07; break;
+                    case "Varna": commision = 0.075; break;
+                    case "Plovdiv": commision = 0.08; break;}}
+            else if (sells <= 10000) {
+                switch (city) {
+                    case "Sofia": commision = 0.08; break;
+                    case "Varna": commision = 0.1; break;
+                    case "Plovdiv": commision = 0.12; break;}}
+            else {
+                switch (city) {
+                    case "Sofia": commision = 0.12; break;
+                    case "Varna": commision = 0.13; break;
+                    case "Plovdiv": commision = 0.145; break;}}
+            double totalCommision = sells * commision;
+            if (totalCommision > 0) {Console.WriteLine($"{totalCommision:f2}");}
+            else {Console.WriteLine("error");}
         }
     }
 }
