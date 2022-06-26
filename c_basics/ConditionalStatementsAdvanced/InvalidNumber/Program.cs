137937a4db8da5dd343d785696d49590677566dd
BobsunPower <bobsunpower@gmail.com>
2022-06-26 07:46:32 +0300

    Completed

--- c_basics/ConditionalStatementsAdvanced/InvalidNumber/Program.cs
+++ c_basics/ConditionalStatementsAdvanced/InvalidNumber/Program.cs
@@ -6,7 +6,9 @@ namespace InvalidNumber
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine());
+            bool valid = (n >= 100 && n <= 200 || n == 0);
+            if (!valid) {Console.WriteLine("invalid");}
         }
     }
 }
