e2436b29eabe099e518aa1cd880416c0dabce4ac
BobsunPower <bobsunpower@gmail.com>
2022-06-27 09:27:32 +0300

    Completed

--- c_basics/ConditionalStatementsAdvanced/SummerOutfit/Program.cs
+++ c_basics/ConditionalStatementsAdvanced/SummerOutfit/Program.cs
@@ -6,7 +6,21 @@ namespace SummerOutfit
     {
         static void Main(string[] args)
         {
-            
+            int temp = int.Parse(Console.ReadLine());
+            string dayPart = Console.ReadLine();
+            string outfit = "";
+            string shoes = "";
+            if (temp >= 10 && temp <= 18) {
+                if (dayPart == "Morning") {outfit = "Sweatshirt"; shoes = "Sneakers";}
+                else if (dayPart == "Afternoon" || dayPart == "Evening") { outfit = "Shirt"; shoes = "Moccasins";}}
+            else if (temp <= 24)
+            {if (dayPart == "Morning" || dayPart == "Evening") {outfit = "Shirt";shoes = "Moccasins";}
+                else if (dayPart == "Afternoon") {outfit = "T-Shirt"; shoes = "Sandals";}}
+            else if (temp >= 25) {
+                if (dayPart == "Morning") {outfit = "T-Shirt"; shoes = "Sandals";}
+                else if (dayPart == "Afternoon") {outfit = "Swim Suit"; shoes = "Barefoot";}
+                else {outfit = "Shirt"; shoes = "Moccasins";}}
+            Console.WriteLine($"It's {temp} degrees, get your {outfit} and {shoes}.");
         }
     }
 }
