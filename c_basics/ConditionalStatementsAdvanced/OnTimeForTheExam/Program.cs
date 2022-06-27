e3c2c10c94788ba4a95528eb9ea8237010827272
BobsunPower <bobsunpower@gmail.com>
2022-06-27 14:20:23 +0300

    Completed

--- c_basics/ConditionalStatementsAdvanced/OnTimeForTheExam/Program.cs
+++ c_basics/ConditionalStatementsAdvanced/OnTimeForTheExam/Program.cs
@@ -6,19 +6,13 @@ namespace OnTimeForTheExam
     {
         static void Main(string[] args)
         {
-            int examHour = int.Parse(Console.ReadLine());
-            int examMinute = int.Parse(Console.ReadLine());
-            int ariveHour = int.Parse(Console.ReadLine());
-            int ariveMinute = int.Parse(Console.ReadLine());
-            int examInMinutes = (examHour * 60) + examMinute;
-            int ariveInMinutes = (ariveHour * 60) + ariveMinute;
-            int dif = examInMinutes - ariveInMinutes;
+            int dif = ((int.Parse(Console.ReadLine()) * 60) + int.Parse(Console.ReadLine())) - ((int.Parse(Console.ReadLine()) * 60) + int.Parse(Console.ReadLine()));
             if (dif >= 0 && dif <= 30) {
                 if (dif > 0) {Console.WriteLine($"On time\n{dif} minutes before the start");}
                 else {Console.WriteLine("On time");}}
             else if (dif > 30) {
-                if (dif > 59) {int hours = dif / 60; int minutes = dif % 60;
-                    Console.WriteLine($"Early\n{hours}:{minutes:d2} hours before the start");}
+                if (dif > 59) {int hrs = dif / 60; int min = dif % 60;
+                    Console.WriteLine($"Early\n{hrs}:{min:d2} hours before the start");}
                 else {Console.WriteLine($"Early\n{dif} minutes before the start");}}
             else if (dif < 0) {dif = Math.Abs(dif);
                 if (dif > 59) {int hours = dif / 60; int minutes = dif % 60;
