a1185ceec9d0b40483c0421c0c0c8489ef23271b
BobsunPower <bobsunpower@gmail.com>
2022-06-27 13:15:03 +0300

    Completed

--- c_basics/ConditionalStatementsAdvanced/OperationsBetweenNumbers/Program.cs
+++ c_basics/ConditionalStatementsAdvanced/OperationsBetweenNumbers/Program.cs
@@ -6,7 +6,30 @@ namespace OperationsBetweenNumbers
     {
         static void Main(string[] args)
         {
-            
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            char sign = char.Parse(Console.ReadLine());
+            string even;
+            double total;
+            if (sign == '+') {total = x + y;
+                if (total % 2 == 0) {even = "even";}
+                else {even = "odd";}
+                Console.WriteLine($"{x} {sign} {y} = {total} - {even}");}
+            else if (sign == '-') {total = x - y;
+                if (total % 2 == 0) {even = "even";}
+                else {even = "odd";}
+                Console.WriteLine($"{x} {sign} {y} = {total} - {even}");}
+            else if (sign == '*') {total = x * y;
+                if (total % 2 == 0) {even = "even";}
+                else {even = "odd";}
+                Console.WriteLine($"{x} {sign} {y} = {total} - {even}");}
+            else if (sign == '/') {if (y != 0) {total = (double)x / y;}
+                else {Console.WriteLine($"Cannot divide {x} by zero"); return;}
+                Console.WriteLine($"{x} {sign} {y} = {total:f2}");}
+            else if (sign == '%') {if (y != 0) {total = x % y;}
+                else {Console.WriteLine($"Cannot divide {x} by zero"); return;}
+                Console.WriteLine($"{x} {sign} {y} = {total}");
+            }
         }
     }
 }
