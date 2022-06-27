629b9c5b5b230b859e94bb1fbbbf5614f1182ea0
BobsunPower <bobsunpower@gmail.com>
2022-06-27 09:36:42 +0300

    Completed

--- c_basics/ConditionalStatementsAdvanced/FishingBoat/Program.cs
+++ c_basics/ConditionalStatementsAdvanced/FishingBoat/Program.cs
@@ -6,7 +6,25 @@ namespace FishingBoat
     {
         static void Main(string[] args)
         {
-            
+            int budget = int.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            int fishermen = int.Parse(Console.ReadLine());
+            double total = 0;
+            if (fishermen <= 6) {
+                if (season == "Spring") {double discount = 3000 * 0.1; total = 3000 - discount;}
+                else if (season == "Summer" || season == "Autumn") {double discount = 4200 * 0.1; total = 4200 - discount;}
+                else if (season == "Winter") {double discount = 2600 * 0.1; total = 2600 - discount;}}
+            else if (fishermen <= 11) {
+                if (season == "Spring") {double discount = 3000 * 0.15; total = 3000 - discount;}
+                else if (season == "Summer" || season == "Autumn") {double discount = 4200 * 0.15; total = 4200 - discount;}
+                else if (season == "Winter") {double discount = 2600 * 0.15; total = 2600 - discount;}}
+            else if (fishermen >= 12) {
+                if (season == "Spring") {double discount = 3000 * 0.25; total = 3000 - discount;}
+                else if (season == "Summer" || season == "Autumn") {double discount = 4200 * 0.25; total = 4200 - discount;}
+                else if (season == "Winter") {double discount = 2600 * 0.25; total = 2600 - discount;}}
+            if (fishermen % 2 == 0 && season != "Autumn") {double discount = total * 0.05; total -= discount;}
+            if (budget >= total) {Console.WriteLine($"Yes! You have {budget - total:f2} leva left.");}
+            else {Console.WriteLine($"Not enough money! You need {total - budget:f2} leva.");}
         }
     }
 }
