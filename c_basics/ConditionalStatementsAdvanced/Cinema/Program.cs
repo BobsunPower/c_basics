c33d390f634129f3d0960eee71b4d458f6db60eb
BobsunPower <bobsunpower@gmail.com>
2022-06-27 09:27:11 +0300

    Completed

--- c_basics/ConditionalStatementsAdvanced/Cinema/Program.cs
+++ c_basics/ConditionalStatementsAdvanced/Cinema/Program.cs
@@ -6,7 +6,17 @@ namespace Cinema
     {
         static void Main(string[] args)
         {
-            
+            string movie = Console.ReadLine();
+            int row = int.Parse(Console.ReadLine());
+            int col = int.Parse(Console.ReadLine());
+            int seats = row * col;
+            double total = 0;
+            switch (movie) {
+                case "Premiere": total = seats * 12.00; break;
+                case "Normal": total = seats * 7.50; break;
+                case "Discount": total = seats * 5.00; break;
+                default: break;}
+            Console.WriteLine($"{total:f2} leva");
         }
     }
 }
