2a62e3ca632a01956f48992a183a150633c78f62
BobsunPower <bobsunpower@gmail.com>
2022-06-28 15:38:47 +0300

    Completed

--- c_basics/ForLoop/Numbers1NWithStep3/Program.cs
+++ c_basics/ForLoop/Numbers1NWithStep3/Program.cs
@@ -6,7 +6,8 @@ namespace Numbers1NWithStep3
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i += 3) {Console.WriteLine(i);}
         }
     }
 }
