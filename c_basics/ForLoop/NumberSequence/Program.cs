1aa4d093fa333b34e5ce32c6c520fef6bde1ef2c
BobsunPower <bobsunpower@gmail.com>
2022-06-28 18:38:54 +0300

    Completed

--- c_basics/ForLoop/NumberSequence/Program.cs
+++ c_basics/ForLoop/NumberSequence/Program.cs
@@ -6,7 +6,15 @@ namespace NumberSequence
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine());
+            double max = double.MinValue; 
+            double min = double.MaxValue; 
+            double cnt = 0;
+            for (int i = 0; i < n; i++)
+            {cnt = int.Parse(Console.ReadLine());
+                if (cnt > max) max = cnt; 
+                if (cnt < min) min = cnt;}
+            Console.WriteLine($"Max number: {max}\nMin number: {min}");
         }
     }
 }
