31a75fe2284682f68e80aa3fa31bb74e32e0d393
BobsunPower <bobsunpower@gmail.com>
2022-06-28 15:42:02 +0300

    Completed

--- c_basics/ForLoop/EvenPowersOf2/Program.cs
+++ c_basics/ForLoop/EvenPowersOf2/Program.cs
@@ -6,7 +6,8 @@ namespace EvenPowersOf2
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= n; i += 2) {Console.WriteLine(Math.Pow(2, i));}
         }
     }
 }
