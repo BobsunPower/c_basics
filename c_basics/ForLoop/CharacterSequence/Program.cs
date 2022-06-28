fde03713b8710591664607deaa6ab0b7dc4986eb
BobsunPower <bobsunpower@gmail.com>
2022-06-28 15:47:30 +0300

    Completed

--- c_basics/ForLoop/CharacterSequence/Program.cs
+++ c_basics/ForLoop/CharacterSequence/Program.cs
@@ -6,7 +6,9 @@ namespace CharacterSequence
     {
         static void Main(string[] args)
         {
-            
+            string txt = Console.ReadLine();
+            int length = txt.Length;
+            for (int i = 0; i < length; i++) {Console.WriteLine(txt[i]);}
         }
     }
 }
