56f31f71b06a184a08604e6925aa00eb85dca289
BobsunPower <bobsunpower@gmail.com>
2022-06-28 18:56:47 +0300

    Completed

--- c_basics/ForLoop/LeftAndRightSum/Program.cs
+++ c_basics/ForLoop/LeftAndRightSum/Program.cs
@@ -6,7 +6,12 @@ namespace LeftAndRightSum
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine());
+            int fst = 0; int snd = 0;
+            for (int i = 0; i < n; i++) {fst += int.Parse(Console.ReadLine());}
+            for (int i = 0; i < n; i++) {snd += int.Parse(Console.ReadLine());}
+            if (fst == snd) {Console.WriteLine($"Yes, sum = {fst}");}
+            else {Console.WriteLine($"No, diff = {Math.Abs(fst - snd)}");};
         }
     }
 }
