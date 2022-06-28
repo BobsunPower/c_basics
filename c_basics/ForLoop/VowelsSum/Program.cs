8a402682bef5ffa0d6cf307a0c11cf6a3904406f
BobsunPower <bobsunpower@gmail.com>
2022-06-28 15:59:09 +0300

    Completed

--- c_basics/ForLoop/VowelsSum/Program.cs
+++ c_basics/ForLoop/VowelsSum/Program.cs
@@ -6,7 +6,17 @@ namespace VowelsSum
     {
         static void Main(string[] args)
         {
-            
+            string txt = Console.ReadLine();
+            int length = txt.Length;
+            int sum = 0;
+            for (int i = 0; i < length; i++) {
+                switch (txt[i]) {
+                    case 'a': sum += 1; break;
+                    case 'e': sum += 2; break;
+                    case 'i': sum += 3; break;
+                    case 'o': sum += 4; break;
+                    case 'u': sum += 5; break;}}
+            Console.WriteLine(sum);
         }
     }
 }
