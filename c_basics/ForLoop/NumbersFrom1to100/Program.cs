65e779e1ef22c6ccc0d8a19f007a14150ebe8911
BobsunPower <bobsunpower@gmail.com>
2022-06-28 13:08:02 +0300

    Completed

--- c_basics/ForLoop/NumbersFrom1to100/Program.cs
+++ c_basics/ForLoop/NumbersFrom1to100/Program.cs
@@ -6,7 +6,7 @@ namespace NumbersFrom1to100
     {
         static void Main(string[] args)
         {
-            
+            for (int i = 1; i <= 100; i++) {Console.WriteLine(i);}
         }
     }
 }
