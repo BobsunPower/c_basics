9960b8fa279db2ef4d74f7b1bec133b6d85cb232
BobsunPower <bobsunpower@gmail.com>
2022-06-28 19:21:29 +0300

    Completed

--- c_basics/ForLoop/OddEvenSum/Program.cs
+++ c_basics/ForLoop/OddEvenSum/Program.cs
@@ -6,7 +6,13 @@ namespace OddEvenSum
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine());
+            int odd = 0; int even = 0; int num = 0;
+            for (int i = 0; i < n; i++) {num = int.Parse(Console.ReadLine());
+                if (i % 2 == 0) {even += num;}
+                else {odd += num;}}
+            if (odd == even) {Console.WriteLine($"Yes\nSum = {odd}");}
+            else {Console.WriteLine($"No\nDiff = {Math.Abs(odd - even)}");}
         }
     }
 }
