0d3f54e78c6b37e815231f1b71b83cc802200a29
BobsunPower <bobsunpower@gmail.com>
2022-06-29 15:37:15 +0300

    Completed

--- c_basics/ForLoop/Salary/Program.cs
+++ c_basics/ForLoop/Salary/Program.cs
@@ -6,7 +6,15 @@ namespace Salary
     {
         static void Main(string[] args)
         {
-            
+            int tabs = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= tabs; i++) {string site = Console.ReadLine();
+                switch (site) {
+                    case "Facebook": salary -= 150; break;
+                    case "Instagram": salary -= 100; break;
+                    case "Reddit": salary -= 50; break;}
+                if (salary <= 0) {Console.WriteLine("You have lost your salary."); return;}}
+            Console.WriteLine($"{salary}");
         }
     }
 }
