e209b627744c16cae612a0401ce7707c4ffce71f
BobsunPower <bobsunpower@gmail.com>
2022-06-29 11:32:29 +0300

    Completed

--- c_basics/ForLoop/HalfSumElement/Program.cs
+++ c_basics/ForLoop/HalfSumElement/Program.cs
@@ -6,7 +6,12 @@ namespace HalfSumElement
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0; int max = int.MinValue;
+            for (int i = 0; i < n; i++)
+            {int add = int.Parse(Console.ReadLine()); sum += add; if (add > max) {max = add;}}
+            if (max == sum - max) { Console.WriteLine($"Yes\nSum = {max}"); }
+            else {Console.WriteLine($"No\nDiff = {Math.Abs(sum - (max * 2))}");}
         }
     }
 }
