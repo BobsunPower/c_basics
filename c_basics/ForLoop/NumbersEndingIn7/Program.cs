f26e0d9d89cba52acac05e9a5d1c73682f9ace1c
BobsunPower <bobsunpower@gmail.com>
2022-06-29 11:16:02 +0300

    Completed

--- c_basics/ForLoop/NumbersEndingIn7/Program.cs
+++ c_basics/ForLoop/NumbersEndingIn7/Program.cs
@@ -6,7 +6,7 @@ namespace NumbersEndingIn7
     {
         static void Main(string[] args)
         {
-            
+            for (int i = 0; i < 1000; i++) {if (i % 10 == 7) {Console.WriteLine(i);}}
         }
     }
 }
