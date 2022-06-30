396340561c456825a00ec7f9f8362233b31c53d7
BobsunPower <bobsunpower@gmail.com>
2022-06-30 09:25:30 +0300

    Completed

--- c_basics/WhileLoop/AccountBalance/Program.cs
+++ c_basics/WhileLoop/AccountBalance/Program.cs
@@ -6,7 +6,13 @@ namespace AccountBalance
     {
         static void Main(string[] args)
         {
-            
+            string add = Console.ReadLine(); double sum = 0;
+            while (add != "NoMoreMoney")
+            {double money = double.Parse(add);
+                if (money < 0) {Console.WriteLine("Invalid operation!"); break;}
+                else {Console.WriteLine($"Increase: {money:f2}"); sum += money;}
+                add = Console.ReadLine();}
+            Console.WriteLine($"Total: {sum:f2}");
         }
     }
 }
