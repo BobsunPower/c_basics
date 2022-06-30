ee6430a10a7f40c9d510cb6b81e503f8ecc521c4
BobsunPower <bobsunpower@gmail.com>
2022-06-30 08:56:51 +0300

    Completed

--- c_basics/WhileLoop/Password/Program.cs
+++ c_basics/WhileLoop/Password/Program.cs
@@ -6,7 +6,11 @@ namespace Password
     {
         static void Main(string[] args)
         {
-            
+            string name = Console.ReadLine();
+            string pwd = Console.ReadLine();
+            string check = "";
+            while (check != pwd) {check = Console.ReadLine();}
+            Console.WriteLine($"Welcome {name}!");
         }
     }
 }
