4291c20f81b45fb41e2094019f0f54bc97727a2a
BobsunPower <bobsunpower@gmail.com>
2022-06-30 08:59:39 +0300

    Completed

--- c_basics/WhileLoop/SumNumbers/Program.cs
+++ c_basics/WhileLoop/SumNumbers/Program.cs
@@ -6,7 +6,10 @@ namespace SumNumbers
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            while (sum < n) {sum += int.Parse(Console.ReadLine());}
+            Console.WriteLine(sum);
         }
     }
 }
