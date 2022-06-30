e281efec282198ea79e8aba65085faa25282d564
BobsunPower <bobsunpower@gmail.com>
2022-06-30 07:40:59 +0300

    Completed

--- c_basics/ForLoop/TennisRanklist/Program.cs
+++ c_basics/ForLoop/TennisRanklist/Program.cs
@@ -6,7 +6,18 @@ namespace TennisRanklist
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine());
+            int ptsBefore = int.Parse(Console.ReadLine());
+            double pts = ptsBefore;
+            double wins = 0;
+            for (int i = 0; i < n; i++) {string result = Console.ReadLine();
+                switch (result)
+                {case "W": pts += 2000; wins++; break;
+                 case "F": pts += 1200; break;
+                 case "SF": pts += 720; break;}}
+            Console.WriteLine($@"Final points: {pts}
+Average points: {Math.Floor((pts - ptsBefore) / n)}
+{wins / n * 100:f2}%");
         }
     }
 }
