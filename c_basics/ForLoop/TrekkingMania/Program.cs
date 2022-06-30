c9ef5ce448db2c28f8dd0336826fb0842e7a60a4
BobsunPower <bobsunpower@gmail.com>
2022-06-30 07:38:08 +0300

    Completed

--- c_basics/ForLoop/TrekkingMania/Program.cs
+++ c_basics/ForLoop/TrekkingMania/Program.cs
@@ -6,7 +6,21 @@ namespace TrekkingMania
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine());
+            int climbers = 0;
+            double[] crews = new double[5];
+            for (int i = 0; i < n; i++)
+            {int ppl = int.Parse(Console.ReadLine()); climbers += ppl;
+                if (ppl < 6) {crews[0] += ppl;}
+                else if (ppl < 13) {crews[1] += ppl;}
+                else if (ppl < 26) {crews[2] += ppl;}
+                else if (ppl < 41) {crews[3] += ppl;}
+                else {crews[4] += ppl;}}
+            Console.WriteLine($@"{(crews[0] / climbers) * 100:f2}%
+{(crews[1] / climbers) * 100:f2}%
+{(crews[2] / climbers) * 100:f2}%
+{(crews[3] / climbers) * 100:f2}%
+{(crews[4] / climbers) * 100:f2}%");
         }
     }
 }
