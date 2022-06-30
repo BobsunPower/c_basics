e373d5f5dcb7e21bd5377e8be169fca1df46516d
BobsunPower <bobsunpower@gmail.com>
2022-06-30 09:42:17 +0300

    Completed

--- c_basics/WhileLoop/Graduation/Program.cs
+++ c_basics/WhileLoop/Graduation/Program.cs
@@ -6,7 +6,12 @@ namespace Graduation
     {
         static void Main(string[] args)
         {
-            
+            string name = Console.ReadLine();
+            double avg = 0; int bad = 0; int ctr = 1;
+            while (ctr <= 12) {double grade = double.Parse(Console.ReadLine());
+                if (grade >= 4) {ctr++; avg += grade;}
+                else {bad++; if (bad == 2) {Console.WriteLine($"{name} has been excluded at {ctr} grade"); return;}}}
+            Console.WriteLine($"{name} graduated. Average grade: {avg / 12:f2}");
         }
     }
 }
