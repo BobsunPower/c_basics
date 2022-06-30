4d7477fd8db22bb80259fd0b0e659ce047d892d1
BobsunPower <bobsunpower@gmail.com>
2022-06-30 09:35:39 +0300

    Completed

--- c_basics/WhileLoop/MaxNumber/Program.cs
+++ c_basics/WhileLoop/MaxNumber/Program.cs
@@ -6,7 +6,10 @@ namespace MaxNumber
     {
         static void Main(string[] args)
         {
-            
+            string n = Console.ReadLine();
+            int max = int.MinValue;
+            while (n != "Stop") { int num = int.Parse(n); if (num > max) { max = num; } n = Console.ReadLine(); }
+            Console.WriteLine(max);
         }
     }
 }
--- c_basics/WhileLoop/MinNumber/Program.cs
+++ c_basics/WhileLoop/MinNumber/Program.cs
@@ -6,10 +6,7 @@ namespace MinNumber
     {
         static void Main(string[] args)
         {
-            string n = Console.ReadLine();
-            int max = int.MinValue;
-            while (n != "Stop") {int num = int.Parse(n); if (num > max) {max = num;} n = Console.ReadLine();}
-            Console.WriteLine(max);
+            
         }
     }
 }
