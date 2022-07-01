d5a502d3da32f92e5f9e8cf2a1e29bb0908b42bf
BobsunPower <bobsunpower@gmail.com>
2022-07-01 16:56:22 +0300

    Completed

--- c_basics/WhileLoop/Walking/Program.cs
+++ c_basics/WhileLoop/Walking/Program.cs
@@ -6,7 +6,12 @@ namespace Walking
     {
         static void Main(string[] args)
         {
-            
+            int goal = 10000; int steps = 0;
+            while (steps < goal) {string add = Console.ReadLine();
+                if (add != "Going home") {int last = int.Parse(add); steps += last;}
+                else {int more = int.Parse(Console.ReadLine()); steps += more; break;}}
+            if (steps >= goal) {Console.WriteLine($"Goal reached! Good job!\n{steps - goal} steps over the goal!");}
+            else {Console.WriteLine($"{goal - steps} more steps to reach goal.");}
         }
     }
 }
