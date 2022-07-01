8ec24adfd73160ef0918fdedd4850eb2accdaf53
BobsunPower <bobsunpower@gmail.com>
2022-07-01 13:34:05 +0300

    Completed

--- c_basics/WhileLoop/ExamPreparation/Program.cs
+++ c_basics/WhileLoop/ExamPreparation/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace ExamPreparation
 {
@@ -6,7 +8,18 @@ namespace ExamPreparation
     {
         static void Main(string[] args)
         {
-            
+            int limit = int.Parse(Console.ReadLine());
+            string cmd = "";
+            int badGrades = 0; string task = "";
+            var grades = new List<double>(); var tasks = new List<string>();
+            while (true) {cmd = Console.ReadLine();
+                if (cmd == "Enough") { Console.WriteLine($@"Average score: {grades.Sum() / grades.Count:f2}
+Number of problems: {tasks.Count}
+Last problem: {task}
+"); break; }
+                int grade = int.Parse(Console.ReadLine()); grades.Add(grade); task = cmd; tasks.Add(task);
+                if (grade < 5) { badGrades++; }
+                if (badGrades == limit) {Console.WriteLine($"You need a break, {limit} poor grades."); break;}}
         }
     }
 }
