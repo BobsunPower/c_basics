bd58531cf6a809b6b27c88a3832ae9781cab67a5
BobsunPower <bobsunpower@gmail.com>
2022-07-01 17:22:00 +0300

    Completed

--- c_basics/WhileLoop/Moving/Program.cs
+++ c_basics/WhileLoop/Moving/Program.cs
@@ -6,7 +6,13 @@ namespace Moving
     {
         static void Main(string[] args)
         {
-            
+            int a = int.Parse(Console.ReadLine()); int b = int.Parse(Console.ReadLine()); int c = int.Parse(Console.ReadLine());
+            int area = a * b * c; int boxes = 0;
+            while (boxes < area) {string cmd = Console.ReadLine();
+                if (cmd != "Done") {int add = int.Parse(cmd); boxes += add;}
+                else {break;}}
+            if (boxes < area) {Console.WriteLine($"{area - boxes} Cubic meters left.");}
+            else {Console.WriteLine($"No more free space! You need {boxes - area} Cubic meters more.");}
         }
     }
 }
