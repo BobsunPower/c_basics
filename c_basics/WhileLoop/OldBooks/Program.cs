e8c364363f953a998e50c19b93a532d8e9c30dca
BobsunPower <bobsunpower@gmail.com>
2022-07-01 13:31:12 +0300

    Completed

--- c_basics/WhileLoop/OldBooks/Program.cs
+++ c_basics/WhileLoop/OldBooks/Program.cs
@@ -6,7 +6,11 @@ namespace OldBooks
     {
         static void Main(string[] args)
         {
-            
+            string book = Console.ReadLine(); string check = Console.ReadLine(); int ctr = 0;
+            while (check != book && check != "No More Books") {check = Console.ReadLine(); ctr++;}
+            if (check == book) {Console.WriteLine($"You checked {ctr} books and found it.");}
+            else { Console.WriteLine($@"The book you search is not here!
+You checked {ctr} books.");}
         }
     }
 }
