e098444b953900127f2022faa4c31f44698c6135
BobsunPower <bobsunpower@gmail.com>
2022-07-01 13:41:27 +0300

    Completed

--- c_basics/WhileLoop/Vacation/Program.cs
+++ c_basics/WhileLoop/Vacation/Program.cs
@@ -6,7 +6,17 @@ namespace Vacation
     {
         static void Main(string[] args)
         {
-            
+            double vacation = double.Parse(Console.ReadLine());
+            double budget = double.Parse(Console.ReadLine());
+            int days = 0; int spended = 0;
+            while (budget < vacation) {string cmd = Console.ReadLine(); double money = double.Parse(Console.ReadLine()); days++;
+                if (cmd == "save")
+                {budget += money; spended = 0;}
+                else {budget -= money; spended++;
+                    if (spended == 5) {break;}}
+                if (budget < 0) {budget = 0;}}
+            if (budget >= vacation) {Console.WriteLine($"You saved the money for {days} days.");}
+            else {Console.WriteLine($"You can't save the money.\n{days}");            }
         }
     }
 }
