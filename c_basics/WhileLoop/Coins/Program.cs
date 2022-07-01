cad4e37626977519dc4d63cebb092c7f2bb49149
BobsunPower <bobsunpower@gmail.com>
2022-07-01 17:04:17 +0300

    Completed

--- c_basics/WhileLoop/Coins/Program.cs
+++ c_basics/WhileLoop/Coins/Program.cs
@@ -6,7 +6,19 @@ namespace Coins
     {
         static void Main(string[] args)
         {
-            
+            double sum = double.Parse(Console.ReadLine()) * 100;
+            int change = (int)sum; int coins = 0;
+            while (change > 0)
+            {if (change >= 200) {change -= 200;}
+                else if (change >= 100) {change -= 100;}
+                else if (change >= 50) {change -= 50;}
+                else if (change >= 20) {change -= 20;}
+                else if (change >= 10) {change -= 10;}
+                else if (change >= 5) {change -= 5;}
+                else if (change >= 2) {change -= 2;}
+                else {change -= 1;}
+                coins++;}
+            Console.WriteLine(coins);
         }
     }
 }
