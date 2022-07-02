d59e47350728c8590cca63d6c145fd324e1c6376
BobsunPower <bobsunpower@gmail.com>
2022-07-02 09:43:11 +0300

    Completed

--- c_basics/NestedLoops/Clock/Program.cs
+++ c_basics/NestedLoops/Clock/Program.cs
@@ -6,7 +6,8 @@ namespace Clock
     {
         static void Main(string[] args)
         {
-            
+            for (int h = 0; h <= 23; h++)
+            for (int m = 0; m <= 59; m++) {Console.WriteLine($"{h}:{m}");}
         }
     }
 }
