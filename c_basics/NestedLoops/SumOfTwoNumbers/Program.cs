7719e91457abaa0df596c8e9f676c91f5f433cd7
BobsunPower <bobsunpower@gmail.com>
2022-07-03 20:20:22 +0300

    Completed

--- c_basics/NestedLoops/SumOfTwoNumbers/Program.cs
+++ c_basics/NestedLoops/SumOfTwoNumbers/Program.cs
@@ -6,7 +6,13 @@ namespace SumOfTwoNumbers
     {
         static void Main(string[] args)
         {
-            
+            int fst = int.Parse(Console.ReadLine()); int snd = int.Parse(Console.ReadLine()); int mgc = int.Parse(Console.ReadLine());
+            int cmb = 0; bool fnd = false;
+            for (int i = fst; i <= snd; i++)
+            {for (int j = fst; j <= snd; j++) {cmb++;
+                    if (i + j == mgc) {Console.WriteLine($"Combination N:{cmb} ({i} + {j} = {mgc})"); fnd = true; break;}}
+                if (fnd == true) {break;}}
+            if (fnd == false) {Console.WriteLine($"{cmb} combinations - neither equals {mgc}");}
         }
     }
 }
