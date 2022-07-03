9343169df68d51bd273036a8aecadf8751e41784
BobsunPower <bobsunpower@gmail.com>
2022-07-03 20:25:53 +0300

    Completed

--- c_basics/NestedLoops/Travelling/Program.cs
+++ c_basics/NestedLoops/Travelling/Program.cs
@@ -6,7 +6,14 @@ namespace Travelling
     {
         static void Main(string[] args)
         {
-            
+            string dtn = "";
+            while ((dtn = Console.ReadLine()) != "End") {double bgt = double.Parse(Console.ReadLine()); double tot = 0;
+                while (bgt >= tot) {string cmd = Console.ReadLine();
+                    if (cmd != "End") {double svg = double.Parse(cmd); tot += svg;}
+                    else {break;}
+                    if (tot >= bgt) {Console.WriteLine($"Going to {dtn}!"); break;}
+                }
+            }
         }
     }
 }
