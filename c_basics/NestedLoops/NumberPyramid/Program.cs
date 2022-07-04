565fb8be88af5aca552a32129646c3a3ef534803
BobsunPower <bobsunpower@gmail.com>
2022-07-04 17:06:04 +0300

    Completed

--- c_basics/NestedLoops/NumberPyramid/Program.cs
+++ c_basics/NestedLoops/NumberPyramid/Program.cs
@@ -6,7 +6,13 @@ namespace NumberPyramid
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine()); int cnt = 1; bool big = false;
+            for (int row = 1; row <= n; row++)
+            {for (int col = 1; col <= row; col++)
+                {if (cnt > n) {big = true; break;} Console.Write($"{cnt} "); cnt++;}
+                if (big) {break;}
+                Console.WriteLine();
+            }
         }
     }
 }
