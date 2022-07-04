3ca958f7750b575698bbc184559e9eb87d297fc1
BobsunPower <bobsunpower@gmail.com>
2022-07-04 17:00:10 +0300

    Completed

--- c_basics/NestedLoops/Building/Program.cs
+++ c_basics/NestedLoops/Building/Program.cs
@@ -6,7 +6,15 @@ namespace Building
     {
         static void Main(string[] args)
         {
-            
+            int frs = int.Parse(Console.ReadLine());
+            int rms = int.Parse(Console.ReadLine());
+            for (int i = frs; i > 0; i--)
+            {for (int j = 0; j < rms; j++)
+                {if (i == frs) {Console.Write($"L{i}{j} ");}
+                    else if (i % 2 == 0) {Console.Write($"O{i}{j} ");}
+                    else {Console.Write($"A{i}{j} ");}}
+                Console.WriteLine();
+            }
         }
     }
 }
