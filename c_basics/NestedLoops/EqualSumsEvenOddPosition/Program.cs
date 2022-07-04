ef2ba0f5fdc0d4b3d6f1bbb8a6a31f10e14a2cc6
BobsunPower <bobsunpower@gmail.com>
2022-07-04 17:09:59 +0300

    Completed

--- c_basics/NestedLoops/EqualSumsEvenOddPosition/Program.cs
+++ c_basics/NestedLoops/EqualSumsEvenOddPosition/Program.cs
@@ -6,7 +6,13 @@ namespace EqualSumsEvenOddPosition
     {
         static void Main(string[] args)
         {
-            
+            int fst = int.Parse(Console.ReadLine()); int snd = int.Parse(Console.ReadLine());
+            for (int i = fst; i <= snd; i++) {string num = i.ToString(); int odd = 0; int evn = 0;
+                for (int j = 0; j < num.Length; j++) {int dgt = int.Parse(num[j].ToString());
+                    if (j % 2 == 0) {evn += dgt;}
+                    else {odd += dgt;}}
+                if (odd == evn) {Console.Write($"{i} ");}
+            }
         }
     }
 }
