faa35405e93e0c9d26a0f51d52b332c149b2dea0
BobsunPower <bobsunpower@gmail.com>
2022-07-05 21:20:36 +0300

    Completed

--- c_basics/NestedLoops/SpecialNumbers/Program.cs
+++ c_basics/NestedLoops/SpecialNumbers/Program.cs
@@ -6,7 +6,18 @@ namespace SpecialNumbers
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine());
+
+            for (int fst = 1; fst <= 9; fst++)
+            {for (int snd = 1; snd <= 9; snd++)
+                {for (int trd = 1; trd <= 9; trd++)
+                    {for (int fth = 1; fth <= 9; fth++)
+                        {if (n % fst == 0 && n % snd == 0 && n % trd == 0 && n % fth == 0)
+                            {Console.Write($"{fst}{snd}{trd}{fth} ");}
+                        }
+                    }
+                }
+            }
         }
     }
 }
