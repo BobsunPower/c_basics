9740977d7ae835a9266699b65b205c323b7a0158
BobsunPower <bobsunpower@gmail.com>
2022-07-05 17:31:10 +0300

    Completed

--- c_basics/NestedLoops/TrainTheTrainers/Program.cs
+++ c_basics/NestedLoops/TrainTheTrainers/Program.cs
@@ -6,7 +6,12 @@ namespace TrainTheTrainers
     {
         static void Main(string[] args)
         {
-            
+            int jury = int.Parse(Console.ReadLine()); int ctr = 0; double all = 0; string presentation;
+            while ((presentation = Console.ReadLine()) != "Finish") {double totalGrades = 0;
+                for (int i = 1; i <= jury; i++)
+                {double grades = double.Parse(Console.ReadLine()); totalGrades += grades; all += grades; ctr++;}
+                Console.WriteLine($"{presentation} - {totalGrades / jury:f2}.");}
+            Console.WriteLine($"Student's final assessment is {all / ctr:f2}.");
         }
     }
 }
